106199529171c4d3b146c7efa2485f1cd5ef7f57
Daniel Stolarski <d.stolarski@o2.pl>
2017-12-07 12:38:18 +0100

    final commit

---
+++ src/Zad05.java
@@ -0,0 +1,12 @@
+public class Zad05 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for(int i = 2; i <= 100; i += 2){
+            System.out.println(i);
+            sum = sum + i;
+        }
+
+        System.out.println("Suma " + sum);
+    }
+}
